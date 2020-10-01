fd775c7c65a0eb44e0fa5cd78481c5844b940db1
Yuri Baldan <yuribbp@gmail.com>
2020-10-01 17:06:26 -0300

    Creating project from the zerogit add .! - PROJECT STARTS HERE

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,19 +16,10 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 	
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
 
-			st.setInt(1, 2);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			e.printStackTrace();
 		} 
 		finally {
 			DB.closeStatement(st);
