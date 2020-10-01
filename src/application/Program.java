9fefe21793739748fb0be3d2df656f3bbed45ac3
Yuri Baldan <yuribbp@gmail.com>
2020-10-01 17:13:38 -0300

    Fake error test

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,36 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		try {
 			conn = DB.getConnection();
-	
 
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate(
+					"UPDATE seller SET BaseSalary = 2090.0 WHERE DepartmentId = 1"
+					);
+
+			int x = 1;
+			if (x < 2) {
+				throw new SQLException("Fake error");
+			}
+
+			int rows2 = st.executeUpdate(
+					"UPDATE seller SET BaseSalary = 3090.0 WHERE DepartmentId = 2"
+					);
+			System.out.println("Rows1: " + rows1);
+			System.out.println("Rows2: " + rows2);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
